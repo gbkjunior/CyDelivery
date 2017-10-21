fde620f4815ef38ed95eca7d329fd89a35933a03
Your Name <you@example.com>
2017-10-21 16:21:14 -0500

    added home page

--- app.js
+++ app.js
@@ -25,6 +25,7 @@ app.use(bodyParser.urlencoded({
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+
 app.use('/', index);
 app.use('/users', users);
 app.use('/home', homepage);
